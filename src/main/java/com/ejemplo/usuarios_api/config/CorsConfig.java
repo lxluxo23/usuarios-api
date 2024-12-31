ed6de5f13cb4c45ec173fc05b53dd141c21c7aff
CondenadoDev <Camiloosoriodev@gmail.com>
2024-12-31 18:35:24 -0300

    prueba con luxete

--- src/main/java/com/ejemplo/usuarios_api/config/CorsConfig.java
+++ src/main/java/com/ejemplo/usuarios_api/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer  {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Permitir todas las rutas de la API
-                .allowedOrigins("https://cobros.myccontadores.cl") // Permitir el origen del frontend
+                .allowedOrigins("https://cobros.myccontadores.cl", "http://localhost:3000") // Permitir el origen del frontend
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos HTTP permitidos
                 .allowedHeaders("*") // Permitir todos los encabezados
                 .allowCredentials(false); // Habilitar credenciales (cookies, tokens, etc.)
